b50675e9a2665efc6d1b2f4b4ec230116834eb22
MartinKalchev <54631309+MartinKalchev@users.noreply.github.com>
2019-10-30 18:50:09 +0200

    Update mainwin.cpp
    Въведох input за смяна на цвета на прозореца според кои бутони са натиснати. (r, g, b)

--- 2D Platformer/2D Platformer/mainwin.cpp
+++ 2D Platformer/2D Platformer/mainwin.cpp
@@ -1,20 +1,56 @@
-#include "SDL.h"
+#include <SDL.h>
 
 using namespace std;
 
 int main(int args, char* argv[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow("Green Screen", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Window* window = NULL;
+	SDL_Renderer* mRender = NULL;
 
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_Init(SDL_INIT_VIDEO);
 
-	SDL_RenderClear(renderer);
+	window = SDL_CreateWindow("Main SDL window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
+	mRender = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	SDL_RenderPresent(renderer);
+	bool isRunning = true;
+	SDL_Event ev;
 
-	SDL_Delay(6000);
+	while (isRunning)
+	{
+		while (SDL_PollEvent(&ev) != 0)
+		{
+			if (ev.type == SDL_QUIT)
+				isRunning = false;
+			else if (ev.type == SDL_KEYDOWN)
+			{
+				switch (ev.key.keysym.sym)
+				{
+				case SDLK_r:
+					SDL_SetRenderDrawColor(mRender, 255, 0, 0, 255);
+					break;
+				case SDLK_g:
+					SDL_SetRenderDrawColor(mRender, 0, 255, 0, 255);
+					break;
+				case SDLK_b:
+					SDL_SetRenderDrawColor(mRender, 0, 0, 255, 255);
+					break;
+				default:
+					SDL_SetRenderDrawColor(mRender, 0, 0, 0, 0);
+				}
+
+
+			}
+		}
+
+		SDL_RenderClear(mRender);
+		SDL_RenderPresent(mRender);
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(mRender);
+	window = NULL;
+	mRender = NULL;
+	SDL_Quit();
 
 	return 0;
 }
